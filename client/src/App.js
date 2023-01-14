826dfabc1006ff6eb4ee5b46395b183e6714f679
Desmond <desmondtekum9@gmail.com>
2023-01-14 11:59:01 +0100

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
-import RightSidebar from '../src/components/RightSidebar.js';
-import LeftSidebar from '../src/components/LeftSidebar.js';
-import MiddleSidebar from '../src/components/MiddleSidebar.js';
-import './App.css';
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="row">
-    <div className='col col-2'> 
-      <LeftSidebar />
-      </div>
-      <div className='col col-6'> 
-      <MiddleSidebar />
-      </div>
-      <div className='col col-4'>
-      <RightSidebar />
-      </div>
-      
-    </div>
-  );
+
+// Import all components
+import Username from './componenents/Username'
+import Profile from './componenents/Profile';
+import Recovery from './componenents/Recovery';
+import Register from './componenents/Register';
+import Password from './componenents/Password';
+import Reset from './componenents/Reset';
+import PageNotFound from './componenents/PageNotFound';
+
+
+// root routes
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Username>Root Route</Username>
+    },
+    {
+        path: '/register',
+        element: <Register>Root Route</Register>
+    },
+    {
+      path: '/password',
+      element: <Password>Root Route</Password>
+  },
+  {
+    path: '*',
+    element: <PageNotFound>Root Route</PageNotFound>
+},
+{
+  path: '/reset',
+  element: <Reset>Root Route</Reset>
+},
+{
+  path: '/recovery',
+  element: <Recovery>Root Route</Recovery>
+},
+{
+  path: '/profile',
+  element: <Profile>Root Route</Profile>
 }
-
-export default App;
+])
+export default function App() {
+  return (
+    <main>
+        <RouterProvider router = {router}></RouterProvider>
+        
+  
+    </main>
+  )
+}
---
+++ client/src/componenents/Username.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function UserName() {
+  return (
+    <div className='container mx-auto'>
+        <div className='flex justify-center item-center h-screen'>
+          <div className='title'>
+           <h4> Hello Again</h4>
+
+          </div>
+
+        </div>
+    </div>
+  )
+}
